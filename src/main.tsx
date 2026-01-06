0961c536cc458a20ba04fa62e615d677f976dffa
Kamalesh <kamalesh3703s@gmail.com>
2026-01-06 12:25:33 +0530

    Delete src directory

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import './lib/aws-config'; // Initialize AWS Amplify
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
